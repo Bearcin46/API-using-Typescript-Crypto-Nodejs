d494b4303c3162708ede17689033183a51e91af3
Bearcin46 <imsweety33@gmail.com>
2024-04-23 18:48:48 +0530

    chore: authentication going on

--- src/controller/authenticaion.ts
+++ src/controller/authenticaion.ts
@@ -1,1 +1,15 @@
 import express from "express";
+import { getUserByEmail, createUser } from "db/users";
+import { random, authentication } from "helpers";
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({ message: "no email or no password" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
