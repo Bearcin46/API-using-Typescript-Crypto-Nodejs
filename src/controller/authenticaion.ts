d2a430675ce259ff8b77b9b59220d2a05dcd406d
Bearcin46 <imsweety33@gmail.com>
2024-04-24 19:09:18 +0530

    chore: login authentication almost completed

--- src/controller/authenticaion.ts
+++ src/controller/authenticaion.ts
@@ -6,6 +6,33 @@ import { random, authentication } from "../helpers";
 export const login = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "no email or password" });
+    }
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+    if (!user) {
+      return res.sendStatus(400);
+    }
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password != expectedHash) {
+      return res.sendStatus(403);
+    }
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("BEARCIN-AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     res.sendStatus(400);
